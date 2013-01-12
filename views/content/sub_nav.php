2c390c125b3175772a6dca6a96c1a002baa35f43
Kyle Lexmond <github@kyl191.net>
2013-01-11 22:40:22 -0500

    Add addQuestion link to submenu

--- views/content/sub_nav.php
+++ views/content/sub_nav.php
@@ -5,4 +5,7 @@
 	<li <?php echo $this->uri->segment(4) == 'create' ? 'class="active"' : '' ?>>
 		<a href="<?php echo site_url(SITE_AREA .'/content/cards/create') ?>">New Quiz</a>
 	</li>
+    <li <?php echo $this->uri->segment(4) == 'addQuestion' ? 'class="active"' : '' ?>>
+		<a href="<?php echo site_url(SITE_AREA .'/content/cards/addQuestion') ?>">Add a Question</a>
+	</li>
 </ul>
