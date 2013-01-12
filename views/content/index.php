767667076d2260ce91d8905fbc4647b1df508785
Kyle Lexmond <github@kyl191.net>
2013-01-11 21:38:00 -0500

    Tab-to-space, and added the Questions column to display the count of questions

--- views/content/index.php
+++ views/content/index.php
@@ -1,53 +1,56 @@
 <h1>Blog Index</h1>
 <p>This is the cards index page</p>
 <div class="admin-box">
-	<h3>Blog Posts</h3>
+    <h3>Blog Posts</h3>
 
-	<?php echo form_open(); ?>
+    <?php echo form_open(); ?>
 
-	<table class="table table-striped">
-		<thead>
-			<tr>
-				<th class="column-check"><input class="check-all" type="checkbox" /></th>
-				<th>Title</th>
-				<th style="width: 10em">Date</th>
-			</tr>
-		</thead>
-		<tfoot>
-			<tr>
-				<td colspan="3">
-					With selected: 
-					<input type="submit" name="submit" class="btn" value="Delete"> 
-				</td>
-			</tr>
-		</tfoot>
-		<tbody>
-		<?php if (isset($posts) && is_array($posts)) :?>
-			<?php foreach ($posts as $post) : ?>
-			<tr>
-				<td><input type="checkbox" name="checked[]" value="<?php echo $post->post_id ?>" /></td>
-				<td>
-					<a href="<?php echo site_url(SITE_AREA .'/content/blog/edit_post/'. $post->post_id) ?>">
-						<?php e($post->title); ?>
-					</a>
-				</td>
-				<td>
-					<?php echo date('M j, Y g:ia'); ?>
-				</td>
-			</tr>
-			<?php endforeach; ?>
-		<?php else: ?>
-			<tr>
-				<td colspan="3">
-					<br/>
-					<div class="alert alert-warning">
-						No Posts found.
-					</div>
-				</td>
-			</tr>
-		<?php endif; ?>
-		</tbody>
-	</table>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th class="column-check"><input class="check-all" type="checkbox" /></th>
+                <th>Title</th>
+                <th style="width: 10em">Username</th>
+                <th style="width: 3em">Questions</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <td colspan="3">
+                    With selected: 
+                    <input type="submit" name="submit" class="btn" value="Delete"> 
+                </td>
+            </tr>
+        </tfoot>
+        <tbody>
+        <?php if (isset($posts) && is_array($posts)) :?>
+            <?php foreach ($posts as $post) : ?>
+            <tr>
+                <td><input type="checkbox" name="checked[]" value="<?php echo $post->id ?>" /></td>
+                <td>
+                    <a href="<?php echo site_url(SITE_AREA .'/content/cards/edit_test/'. $post->id) ?>">
+                        <?php e($post->title); ?>
+                    </a>
+                </td>
+                <td>
+                    <?php echo date('M j, Y g:ia'); ?>
+                </td>
+                <td>
+                    <?php echo $post->numQuestions; ?>
+            </tr>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <tr>
+                <td colspan="3">
+                    <br/>
+                    <div class="alert alert-warning">
+                        No Posts found.
+                    </div>
+                </td>
+            </tr>
+        <?php endif; ?>
+        </tbody>
+    </table>
 
-	<?php echo form_close(); ?>
+    <?php echo form_close(); ?>
 </div>
