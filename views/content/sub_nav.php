0f950f5ce43acade44d255f8528433a1f5eb19bf
Kyle Lexmond <github@kyl191.net>
2013-01-11 22:48:04 -0500

    Tab to space, and added list_questions link

--- views/content/sub_nav.php
+++ views/content/sub_nav.php
@@ -1,11 +1,14 @@
 <ul class="nav nav-pills">
-	<li <?php echo $this->uri->segment(4) == '' ? 'class="active"' : '' ?>>
-		<a href="<?php echo site_url(SITE_AREA .'/content/cards') ?>">List of Quizzes</a>
-	</li>
-	<li <?php echo $this->uri->segment(4) == 'create' ? 'class="active"' : '' ?>>
-		<a href="<?php echo site_url(SITE_AREA .'/content/cards/create') ?>">New Quiz</a>
-	</li>
-    <li <?php echo $this->uri->segment(4) == 'addQuestion' ? 'class="active"' : '' ?>>
-		<a href="<?php echo site_url(SITE_AREA .'/content/cards/addQuestion') ?>">Add a Question</a>
-	</li>
+    <li <?php echo $this->uri->segment(4) == '' ? 'class="active"' : '' ?>>
+        <a href="<?php echo site_url(SITE_AREA .'/content/cards') ?>">List of Quizzes</a>
+    </li>
+    <li <?php echo $this->uri->segment(4) == 'create' ? 'class="active"' : '' ?>>
+        <a href="<?php echo site_url(SITE_AREA .'/content/cards/create') ?>">New Quiz</a>
+    </li>
+    <li <?php echo $this->uri->segment(4) == 'list_questions' ? 'class="active"' : '' ?>>
+        <a href="<?php echo site_url(SITE_AREA .'/content/cards/list_questions') ?>">List of Questions</a>
+    </li>
+    <li <?php echo $this->uri->segment(4) == 'add_question' ? 'class="active"' : '' ?>>
+        <a href="<?php echo site_url(SITE_AREA .'/content/cards/add_question') ?>">Add a Question</a>
+    </li>
 </ul>
