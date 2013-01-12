a0821cec48f957558067a84baa667ecc2db053ee
Kyle Lexmond <github@kyl191.net>
2013-01-11 20:16:29 -0500

    Tab-to-space, and fix EOL as DOS/Windows

--- controllers/blog.php
+++ controllers/blog.php
@@ -1,24 +1,27 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Blog extends Front_Controller {
+class Cards extends Front_Controller {
 
-	public function __construct() 
-	{ 
-		parent::__construct();
-		
-		$this->load->model('post_model');
-	}
-	
-	//--------------------------------------------------------------------
-	
-	public function index() 
-	{
-		$this->load->helper('typography');
-	
-		Template::set('posts', $this->post_model->order_by('created_on', 'asc')->limit(5)->find_all());
-	
-		Template::render();
-	}
-	
-	//--------------------------------------------------------------------
-	
+    public function __construct() 
+    { 
+        parent::__construct();
+        
+        $this->load->model('test_model');
+        $this->load->model('question_model');
+        $this->load->model('answer_model');
+        
+    }
+    
+    //--------------------------------------------------------------------
+    
+    public function index() 
+    {
+        $this->load->helper('typography');
+    
+        Template::set('posts', $this->test_model->order_by('id', 'asc')->find_all());
+    
+        Template::render();
+    }
+    
+    //--------------------------------------------------------------------
+    
 }
