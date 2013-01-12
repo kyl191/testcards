7ba662e042c0c113dc916198f806b4188e53cf37
Kyle Lexmond <github@kyl191.net>
2013-01-11 19:42:11 -0500

    Model the database tables

--- models/post_model.php
+++ models/post_model.php
@@ -1,13 +1,30 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Post_model extends MY_Model {
+class test_model extends MY_Model {
 
-	protected $table		= 'posts';
-	protected $key			= 'post_id';
-	protected $set_created	= true;
-	protected $set_modified	= true;
-	protected $soft_deletes	= true;
-	protected $date_format	= 'datetime';
+    protected $table        = 'tests';
+    protected $key          = 'id';
+    protected $set_created  = false;
+    protected $set_modified = false;
+    protected $soft_deletes = false;
+    //---------------------------------------------------------------
+}
 
-	//---------------------------------------------------------------
+class question_model extends MY_Model {
 
+    protected $table        = 'questions';
+    protected $key          = 'id';
+    protected $set_created  = false;
+    protected $set_modified = false;
+    protected $soft_deletes = false;
+    //---------------------------------------------------------------
+}
+
+class answer_model extends MY_Model {
+
+    protected $table        = 'answers';
+    protected $key          = 'id';
+    protected $set_created  = false;
+    protected $set_modified = false;
+    protected $soft_deletes = false;
+    //---------------------------------------------------------------
 }
