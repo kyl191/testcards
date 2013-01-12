50e8af31520899e3ba59a19880117a556c531636
Kyle Lexmond <github@kyl191.net>
2013-01-12 14:05:48 -0500

    A few changes

--- views/start_quiz.php
+++ views/start_quiz.php
@@ -16,12 +16,12 @@
 <div class="clearfloat"></div>
 <input type="radio" name="itemquestion" id="3" onclick="validate(3);"><label id="label3" for="3">Test label 3</label>
 <div class="clearfloat"></div>
-<input type="button" id="nextbutton" onclick="nextQuestion();" value="Next Question">
+<input type="button" id="nextbutton" onclick="nextQuestion(questions);" value="Next Question">
 <input type="button" id="endbutton" onclick="showSummary();" value="End now">
   </div>
   <div class="clearfloat"></div>
   </div>
 <script type="text/javascript"
-  src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+  src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=default">
 </script>
 
