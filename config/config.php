9474379c0847d6d874076537353098a9d07fce01
Kyle Lexmond <github@kyl191.net>
2013-01-11 19:21:24 -0500

    Switch out the config too

--- config/config.php
+++ config/config.php
@@ -1,15 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-	$config['module_config'] = array(
-	    'name'          => 'Blog',          
-	    'description'   => 'A Simple Blog Example',
-	    'author'        => 'Your Name',     
-	    'homepage'      => 'http://testcards.kyl191.net',    
-	    'version'       => '0.0.1',         
-	    'menu'          => array(           
-	        'context'   => 'path/to/view'
-	    ),
-	    'weights'		=> array(
-	    	'context'	=> 0
-	    )
-	);
-    
+    $config['module_config'] = array(
+        'name'          => 'Cards',
+        'description'   => 'The Testcards Module',
+        'author'        => 'Kyle Lexmond',
+        'homepage'      => 'http://testcards.kyl191.net',
+        'version'       => '0.0.1',
+        'menu'          => array(
+            'context'   => 'path/to/view'
+        ),
+        'weights'       => array(
+            'context'   => 0
+        )
+    );
