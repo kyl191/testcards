a9e9f7305369fed412dcd60076c867f025879b5f
Kyle Lexmond <github@kyl191.net>
2013-01-11 20:54:53 -0500

    Update the nav menu with the new menu items

--- views/content/sub_nav.php
+++ views/content/sub_nav.php
@@ -1,8 +1,8 @@
 <ul class="nav nav-pills">
 	<li <?php echo $this->uri->segment(4) == '' ? 'class="active"' : '' ?>>
-		<a href="<?php echo site_url(SITE_AREA .'/content/blog') ?>">Posts</a>
+		<a href="<?php echo site_url(SITE_AREA .'/content/cards') ?>">List of Quizzes</a>
 	</li>
 	<li <?php echo $this->uri->segment(4) == 'create' ? 'class="active"' : '' ?>>
-		<a href="<?php echo site_url(SITE_AREA .'/content/blog/create') ?>">New Post</a>
+		<a href="<?php echo site_url(SITE_AREA .'/content/cards/create') ?>">New Quiz</a>
 	</li>
 </ul>
