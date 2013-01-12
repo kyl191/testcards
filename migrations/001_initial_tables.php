adc0c23032ea3acd0bd9716af8a0f0e79b32fb4e
Kyle Lexmond <github@kyl191.net>
2013-01-11 19:19:36 -0500

    Create our database

--- migrations/001_initial_tables.php
+++ migrations/001_initial_tables.php
@@ -1,1 +1,61 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');class Migration_Initial_tables extends Migration {	public function up() 	{		$this->load->dbforge();				$this->dbforge->add_field('post_id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT');		$this->dbforge->add_field('title VARCHAR(255) NOT NULL');		$this->dbforge->add_field('slug VARCHAR(255) NOT NULL');		$this->dbforge->add_field('body TEXT NULL');		$this->dbforge->add_field('created_on DATETIME NOT NULL');		$this->dbforge->add_field('modified_on DATETIME NULL');		$this->dbforge->add_field('deleted TINYINT(1) NOT NULL DEFAULT 0');		$this->dbforge->add_key('post_id', TRUE);				$this->dbforge->create_table('posts');	}		//--------------------------------------------------------------------		public function down() 	{		$this->load->dbforge();				$this->dbforge->drop_table('posts');	}		//--------------------------------------------------------------------	}
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Migration_Initial_tables extends Migration {
+
+    public function up() {
+        // From http://ellislab.com/forums/viewreply/978554/
+        // CI doesn't support foreign keys, so we have to manually issue the query
+        // Create the tests table first.
+        // Note that we're referencing bf_users - if another prefix is used, this will fail
+        $this->db->query('
+CREATE TABLE `tests` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+ `description` text NOT NULL,
+ `owner` int(11) NOT NULL,
+ `image` varchar(255) NOT NULL,
+ PRIMARY KEY (`id`),
+ KEY `owner` (`owner`),
+ CONSTRAINT `tests_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `bf_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+        
+        // Next, do the questions since that depends on the tests
+        $this->db->query('
+CREATE TABLE `questions` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `parent_test` int(11) NOT NULL,
+ `question` text NOT NULL,
+ `modified` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ PRIMARY KEY (`id`),
+ KEY `parent_test` (`parent_test`),
+ CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`parent_test`) REFERENCES `tests` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+        
+        // Finally, the answers, since it depends on the questions
+        $this->db->query('
+CREATE TABLE `answers` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `answer` text NOT NULL,
+ `correct` tinyint(1) NOT NULL,
+ `parent_question` int(11) NOT NULL,
+ `modified` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ PRIMARY KEY (`id`),
+ KEY `parent_question` (`parent_question`),
+ CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`parent_question`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+    }
+    
+    
+    //--------------------------------------------------------------------
+    
+    public function down() 
+    {
+        $this->load->dbforge();
+        
+        $this->dbforge->drop_table('answers');
+        $this->dbforge->drop_table('questions');
+        $this->dbforge->drop_table('tests');
+    }
+    
+    //--------------------------------------------------------------------
+    
+}
