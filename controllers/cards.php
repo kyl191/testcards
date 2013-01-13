d63b46233d0b4db9a5d52b463d031b2bf9373661
Kyle Lexmond <github@kyl191.net>
2013-01-13 00:40:14 -0500

    Randomize the order of the questions in the generated JSON file

--- controllers/cards.php
+++ controllers/cards.php
@@ -46,7 +46,7 @@ class Cards extends Front_Controller {
         $questions = $this->db->query("select id, question from bf_questions WHERE parent_test =". $id)->result_array();
         for($i = 0; $i < count($questions); $i++){
             $data[$i]['question'] = $questions[$i]['question'];
-            $answers = $this->db->query('select answer, correct from bf_answers WHERE parent_question ='. $questions[$i]['id'])->result_array();
+            $answers = $this->db->query('select answer, correct from bf_answers WHERE parent_question ='. $questions[$i]['id']." ORDER BY RAND()")->result_array();
             for($j = 1; $j <= count($answers); $j++){
                 $data[$i]['label'.$j] = $answers[$j-1]['answer'];
                 if ($answers[$j-1]['correct'] == 1){
