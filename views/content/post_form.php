054e1fc2c05190045b4a95b57618691f0fb7c4b9
Kyle Lexmond <github@kyl191.net>
2013-05-05 07:43:14 -0400

    Add the question index code

--- views/content/post_form.php
+++ views/content/post_form.php
@@ -7,6 +7,7 @@
         <label for="title">Title</label>
         <div class="controls">
             <input type="text" name="title" class="input-xxlarge" value="<?php echo set_value('title', isset($post) ? $post->title : ''); ?>" />
+
             <?php if (form_error('title')) echo '<span class="help-inline">'. form_error('title') .'</span>'; ?>
         </div>
     </div>
@@ -18,8 +19,41 @@
             <input type="text" name="description" class="input-xxlarge" value="<?php echo set_value('description', isset($post) ? $post->description : ''); ?>" />
         </div>
     </div>
-    
+
+    <?php if (isset($questions) && is_array($questions) && !empty($questions)) :?>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th class="column-check"><input class="check-all" type="checkbox" /></th>
+                    <th>Question</th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <td colspan="2">
+                        With selected: 
+                        <input type="submit" name="delete" class="btn" value="Delete"> 
+                    </td>
+                </tr>
+            </tfoot>
+            <tbody>
+            <?php foreach ($questions as $question) : ?>
+                <tr>
+                    <td><input type="checkbox" name="checked[]" value="<?php echo $question['id'] ?>" /></td>
+                    <td>
+                        <a href="<?php echo site_url(SITE_AREA .'/content/cards/edit_question/'. $question['id']) ?>">
+                            <?php e($question['question']); ?>
+                        </a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
     <div class="form-actions">
+        <?php if (isset($questions) && is_array($questions) && !empty($questions)) :?>
+            <input type="submit" name="add" class="btn btn-primary" value="Add a question" /> or 
+        <?php endif; ?>
         <input type="submit" name="submit" class="btn btn-primary" value="Save Quiz" />
         or <a href="<?php echo site_url(SITE_AREA .'/content/cards') ?>">Cancel</a>
     </div>
