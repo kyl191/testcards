5facfa6b04f256bb61bbcd401962207626f510e5
Kyle Lexmond <github@kyl191.net>
2013-05-05 08:47:30 -0400

    User_controller doesn't exist, shois actually authenticated_controller

--- controllers/content.php
+++ controllers/content.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    class Content extends User_Controller {
+    class Content extends Authenticated_Controller {
         
 
         
