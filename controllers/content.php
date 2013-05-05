b95df5e607a42154ec6f45e0bc144542bd104fe4
Kyle Lexmond <github@kyl191.net>
2013-05-05 07:43:50 -0400

    Drop down permissions so any registered user can use it

--- controllers/content.php
+++ controllers/content.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    class Content extends Admin_Controller {
+    class Content extends User_Controller {
         
         public function index() 
         {
